6acbaf12c653531e8d65f682f4603e36c6d565ba
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-12-15 00:59:23 +0900

    os.stat

--- bin/p/ls.py
+++ bin/p/ls.py
@@ -32,9 +32,17 @@ def list(path:str='.'):
     result = os.listdir(path)
     result = sorted(result)
     for i in range(len(result)):
+        stat = os.stat(result[i])
         result[i] = {
             'name': result[i],
             'type': get_type(result[i]),
             'size': get_size(result[i]),
+            'inode': stat['st_ino'],
+            'atime': stat['st_atime'],
+            'mtime': stat['st_mtime'],
+            'ctime': stat['st_ctime'],
+            'uid': stat['st_uid'],
+            'gid': stat['st_gid'],
+            'stat': stat,
         }
     return result
