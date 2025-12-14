fb5e8d2b9e672370307fb912e7ceb0fcb3d964b8
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-12-15 00:58:58 +0900

    def get_type

--- bin/p/ls.py
+++ bin/p/ls.py
@@ -1,10 +1,29 @@
 import os,sys
 
+def get_type(path:str='.'):
+    if False:
+        pass
+    elif os.path.isfile(path):
+        return 'file'
+    elif os.path.isdir(path):
+        return 'dir'
+    elif os.path.islink(path):
+        return 'link'
+    elif os.path.isjunction(path):
+        return 'junction'
+    elif os.path.ismount(path):
+        return 'mountpoint'
+    elif os.path.isdevdrive(path):
+        return 'drive'
+    else:
+        return 'unknown'
+
 def list(path:str='.'):
     result = os.listdir(path)
     result = sorted(result)
     for i in range(len(result)):
         result[i] = {
             'name': result[i],
+            'type': get_type(result[i]),
         }
     return result
