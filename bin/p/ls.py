de5a49d38f607d79ce2c9bfef99aba19fb54b907
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-12-15 00:58:08 +0900

    Update ls.py

--- bin/p/ls.py
+++ bin/p/ls.py
@@ -3,4 +3,8 @@ import os,sys
 def list(path:str='.'):
     result = os.listdir(path)
     result = sorted(result)
+    for i in range(len(result)):
+        result[i] = {
+            'name': result[i],
+        }
     return result
